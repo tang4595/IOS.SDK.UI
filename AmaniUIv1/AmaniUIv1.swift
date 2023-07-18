5a7c6c29e9e357523fc91dc42dca93daeb2d65e6
Deniz Can <deniz@amani.tech>
2023-07-18 10:37:47 +0300

    update the initialization methods

--- AmaniUIv1/AmaniUIv1.swift
+++ AmaniUIv1/AmaniUIv1.swift
@@ -15,10 +15,6 @@ public class AmaniUIv1 {
   /// General Application Config
   /// This property represents the delegate methods.
   public weak var delegate: AmaniUIDelegate?
-  public var country: String?
-  public var nviData: NviModel?
-  public var location: CLLocation?
-  
   // MARK: - Private setups
   /// Home Screen is the initial view controller
   private var initialVC: HomeViewController?
@@ -41,41 +37,53 @@ public class AmaniUIv1 {
   private var bundle: Bundle!
   private var customerRespData: CustomerResponseModel? = nil
   
+  private var server: String? = nil
+  private var token: String? = nil
+  private var userName: String? = nil
+  private var password: String? = nil
+  private var sharedSecret: String? = nil
+  private var customer: CustomerRequestModel? = nil
+  private var language: String = "tr"
+  private var apiVersion: ApiVersions = .v2
+  public var country: String? = nil
+  public var nviData: NviModel? = nil
+  public var location: CLLocation? = nil
+  
   /**
    This method used to get SDK bundle
    - returns: Bundle
    */
   func getBundle() -> Bundle {
-      return self.bundle
+    return self.bundle
   }
   
   public init() {
-      setBundle()
+    setBundle()
   }
   
-  public func setNvi(nvi:NviModel){
-    nviData = nvi
-  }
+  //  public func setNvi(nvi:NviModel){
+  //    nviData = nvi
+  //  }
   public func getNvi()->NviModel?{
     return nviData
   }
-    
-    /**
-     This method set up the SDK bundle
-     
-     */
-    private func setBundle() {
-        if let bundle = Bundle(path: "AmaniUIv1.bundle") {
-            self.bundle = bundle
-        } else if let path = Bundle(for: AmaniBundleLocator.self).path(forResource: "AmaniUIv1", ofType: "bundle"),
-                  let bundle = Bundle(path: path)  {
-            self.bundle = bundle
-        } else {
-            let bundle = Bundle(for: AmaniBundleLocator.self)
-            self.bundle = bundle
-        }
+  
+  /**
+   This method set up the SDK bundle
+   
+   */
+  private func setBundle() {
+    if let bundle = Bundle(path: "AmaniUIv1.bundle") {
+      self.bundle = bundle
+    } else if let path = Bundle(for: AmaniBundleLocator.self).path(forResource: "AmaniUIv1", ofType: "bundle"),
+              let bundle = Bundle(path: path)  {
+      self.bundle = bundle
+    } else {
+      let bundle = Bundle(for: AmaniBundleLocator.self)
+      self.bundle = bundle
     }
-    
+  }
+  
   
   /**
    This method set the SDK configuration
@@ -99,27 +107,19 @@ public class AmaniUIv1 {
     nviModel: NviModel? = nil,
     country: String? = nil,
     location: CLLocation? = nil,
-    apiVersion:ApiVersions = .v2,
-    completion: @escaping (CustomerResponseModel?, NetworkError?) -> ()
+    apiVersion:ApiVersions = .v2
   ) {
-    self.nviData = nviModel
+    self.server = server
+    self.token = token
+    self.sharedSecret = sharedSecret
+    self.useGeolocation = useGeoLocation
+    self.customer = customer
     self.country = country
+    self.nviData = nviModel
     self.location = location
-    Amani.sharedInstance.initAmani(server: server, token: token, sharedSecret: sharedSecret, customer: customer, language: language,apiVersion: apiVersion) {(customerModel, error) in
-      do {
-        self.config = try Amani.sharedInstance.appConfig().getApplicationConfig()
-      } catch let error {
-        print("Error while fetching app configuration \(error)")
-      }
-      if let customerResponseModel = customerModel {
-        self.customerRespData = customerResponseModel
-      }
-      completion(customerModel, error)
-    }
-    
   }
   
-   /**
+  /**
    This method set the SDK configuration
    - parameter server: Server
    - parameter email: String
@@ -143,69 +143,63 @@ public class AmaniUIv1 {
     nviModel: NviModel? = nil,
     country: String? = nil,
     location: CLLocation? = nil,
-    apiVersion:ApiVersions = .v2,
-    completion: @escaping (CustomerResponseModel?, NetworkError?) -> ()
+    apiVersion:ApiVersions = .v2
   ) {
-    self.nviData = nviModel
+    self.server = server
+    self.userName = userName
+    self.password = password
+    self.sharedSecret = sharedSecret
+    self.useGeolocation = useGeoLocation
+    self.customer = customer
     self.country = country
+    self.nviData = nviModel
     self.location = location
-
-    sharedSDKInstance.initAmani(server: server, userName: userName, password: password, sharedSecret: sharedSecret, customer: customer, language: language,apiVersion: apiVersion) {(customerModel, error) in
-      
-      Amani.sharedInstance.appConfig().fetchAppConfig(completion: { [weak self] config, error in
-        if error == nil {
-          self?.config = config
-          
-        } else {
+  }
+  
+  public func showSDK(on parentViewController: UIViewController,
+                      completion: ((CustomerResponseModel?, NetworkError?) -> ())?
+  ) {
+    parentVC = parentViewController
+    
+    if (userName != nil && password != nil) {
+      Amani.sharedInstance.initAmani(server: server!, userName: self.userName!, password: self.password!, sharedSecret: sharedSecret, customer: customer!, language: language, apiVersion: apiVersion) {[weak self] (customerModel, error) in
+        guard let self = self else {return}
+        do {
+          self.config = try Amani.sharedInstance.appConfig().getApplicationConfig()
+        } catch let error {
           print("Error while fetching app configuration \(error)")
         }
-      })
-      
-      if let customerResponseModel = customerModel {
-        self.customerRespData = customerResponseModel
+        if let customerResponseModel = customerModel {
+          self.customerRespData = customerResponseModel
+        }
+        
+        if let comp = completion {
+          updateConfig()
+          comp(customerModel, error)
+        }
       }
-      completion(customerModel, error)
+    } else if (token != nil){
+      
+      Amani.sharedInstance.initAmani(server: server!, token: token!, sharedSecret: sharedSecret!, customer: customer!, language: language, apiVersion: apiVersion) {[weak self] (customerModel, error) in
+        guard let self = self else {return}
+        do {
+          self.config = try Amani.sharedInstance.appConfig().getApplicationConfig()
+        } catch let error {
+          print("Error while fetching app configuration \(error)")
+        }
+        if let customerResponseModel = customerModel {
+          self.customerRespData = customerResponseModel
+        }
+        
+        if let comp = completion {
+          comp(customerModel, error)
+          updateConfig()
+        }
+      }
     }
   }
   
-  public func showSDK(on parentViewController: UIViewController) {
-    parentVC = parentViewController
-    
-    // Updating the configuration for cases like if user exits the sdk view and restarts the KYC process
-    updateConfig { [weak self] () in
-      guard let self else {return}
-      DispatchQueue.main.async {
-        self.initialVC = HomeViewController(nibName: String(describing: HomeViewController.self), bundle: Bundle(for: HomeViewController.self))
-        self.initialVC!.bind(customerData: self.customerRespData!)
-        self.sharedSDKInstance.setDelegate(delegate: self.initialVC!)
-
-        
-        self.sdkNavigationController = UINavigationController(rootViewController: self.initialVC!)
-        self.sdkNavigationController?.modalPresentationStyle = .fullScreen
-        if let navController = parentViewController.navigationController {
-          self.clientNavColor = navController.navigationBar.barTintColor
-          self.clientNavBackColor = navController.navigationBar.backgroundColor
-        }
-        
-        // Setting the app theme here will ensure it runs correctly on the first time. Otherwise it won't have the correct theme
-        // on launch
-        /// TODO: Make this patch on original v1 code.
-        self.setAppTheme(model: self.config?.generalconfigs!, onVC: self.initialVC!)
-        
-        // Adding shadow to NavigationBar
-        self.sdkNavigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
-        self.sdkNavigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-        self.sdkNavigationController?.navigationBar.layer.shadowRadius = 4.0
-        self.sdkNavigationController?.navigationBar.layer.shadowOpacity = 0.4
-        self.sdkNavigationController?.navigationBar.layer.masksToBounds = false
-        
-        // Show the SDK!
-        parentViewController.present(self.sdkNavigationController!, animated: true)
-      }
-    }
-
-
-  }
+  
   
   public func setDelegate(delegate: AmaniUIDelegate) {
     self.delegate = delegate
@@ -224,18 +218,49 @@ public class AmaniUIv1 {
   }
   
   // MARK: - internal methods
-  internal func updateConfig(cb:@escaping VoidCallback) {
+  internal func updateConfig() {
     Amani.sharedInstance.appConfig().fetchAppConfig {[weak self] (newConfig, error) in
       if let newConfig = newConfig {
         if let self = self {
           self.config = newConfig
-          cb()
+          self.uptateSDKView()
         }
       } else {
         if let error = error {
           print("Failed to update the application configuration. Reason: \(String(describing: error.error_code)): \(String(describing: error.error_message))")
         }
       }
+    }
+  }
+  
+  private func uptateSDKView() {
+    DispatchQueue.main.async {
+      self.initialVC = HomeViewController(nibName: String(describing: HomeViewController.self), bundle: Bundle(for: HomeViewController.self))
+      self.initialVC!.bind(customerData: self.customerRespData!)
+      self.sharedSDKInstance.setDelegate(delegate: self.initialVC!)
+      
+      
+      self.sdkNavigationController = UINavigationController(rootViewController: self.initialVC!)
+      self.sdkNavigationController?.modalPresentationStyle = .fullScreen
+      
+      if let navController = self.parentVC?.navigationController {
+        self.clientNavColor = navController.navigationBar.barTintColor
+        self.clientNavBackColor = navController.navigationBar.backgroundColor
+      }
+      
+      // Setting the app theme here will ensure it runs correctly on the first time. Otherwise it won't have the correct theme
+      // on launch
+      self.setAppTheme(model: self.config?.generalconfigs!, onVC: self.initialVC!)
+      
+      // Adding shadow to NavigationBar
+      self.sdkNavigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
+      self.sdkNavigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+      self.sdkNavigationController?.navigationBar.layer.shadowRadius = 4.0
+      self.sdkNavigationController?.navigationBar.layer.shadowOpacity = 0.4
+      self.sdkNavigationController?.navigationBar.layer.masksToBounds = false
+      
+      // Show the SDK!
+      self.parentVC?.present(self.sdkNavigationController!, animated: true)
     }
   }
   
@@ -270,7 +295,7 @@ public class AmaniUIv1 {
       onVC.setNavigationLeftButton(TintColor: model.topBarFontColor ?? "000000")
       
       onVC.setNavigationBarWith(title: model.mainTitleText!, textColor: UIColor(hexString: model.topBarFontColor ?? "000000"))
-//      onVC.headView.layer.cornerRadius = 25
+      //      onVC.headView.layer.cornerRadius = 25
       onVC.headView.backgroundColor = UIColor(hexString: model.appBackground ?? "0F2435")
       onVC.setBackgroundColorOfTableView(color: UIColor(hexString: model.appBackground ?? "253C59"))
     }
@@ -296,3 +321,4 @@ public class AmaniUIv1 {
 //  }
 //
 //}
+
